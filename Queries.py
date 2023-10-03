3837faa4b19e358e04425b7edeafd3516b45ede5
DESKTOP-5HJ9E7H\Nyila <gabor.nyilas@codecool.com>
2023-10-03 15:27:21 +0200

    modify Queries.py

--- Queries.py
+++ Queries.py
@@ -4,13 +4,7 @@ client = MongoClient('localhost', 27017)
 db = client['customer']
 collection = db['customer_collection']
 
-print('customers with a name containing "an":')
 
-found_customers = collection.find({"name":  {"$regex": "an"}})
-for customer in found_customers:
-    print(customer)
-
-print()
 
 # Update the balance of everybody with an increment os $200
 collection.update_many(
@@ -21,4 +15,15 @@ collection.update_many(
 print('All customers with increased balance:')
 for customer in collection.find():
     print(customer)
+
+
+
+print('customers with a name containing "an":')
+
+found_customers = collection.find({"name":  {"$regex": "an"}})
+for customer in found_customers:
+    print(customer)
+
+print()
+
 
