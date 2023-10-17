98ee4c29cd7b1cd2ba175ce710b89f4ca06b929a
Gábor Nyilas <99878203+GoldFish-Codecool@users.noreply.github.com>
2023-10-17 11:07:45 +0000

    Laf-way with LogBook

--- Gabor/LogBook.py
+++ Gabor/LogBook.py
@@ -1,17 +1,27 @@
 def add_log_entry():
-    pass
+    log_file = open("log.txt", "a+")
+    log_entry = input("Log entry: ")
+    log_file.write(log_entry + "\n")
+    log_file.close()
+
 
 def print_log_entries():
-    pass
+    log_file = open("log.txt", "r")
+    for line in log_file:
+        print(line)
+    log_file.close()
+
 
 def menu():
     print("1. Enter log entry")
     print("2. Show all entries")
     print("3. Quit")
-    choice = int(input("What is your choice"))
+    choice = int(input("What is your choice\n"))
     return choice
 
+
 if __name__=="__main__":
+
     choice = menu()
     while choice != 3:
         if choice == 1:
