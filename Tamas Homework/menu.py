e543e904c6b9888a85c4f47532f1b95692e522f6
Gábor Nyilas <99878203+GoldFish-Codecool@users.noreply.github.com>
2023-10-24 13:15:56 +0200

    Menu

---
+++ Tamas Homework/menu.py
@@ -0,0 +1,9 @@
+# Gabor
+def menu():
+  print("1. Count sentences")
+  print("2. Count words")
+  print("3. Average word length")
+  print("4. Most common word")
+  print("5. Quit")
+  choice = int(input("What is your choice\n\n"))
+  return choice
