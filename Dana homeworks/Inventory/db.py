400aa045611692734d8cbd18559d8334df4f938f
DaniellaRigo <146843246+DaniellaRigo@users.noreply.github.com>
2023-11-07 10:05:52 +0000

    Dana_9th_class

--- Dana homeworks/Inventory/db.py
+++ Dana homeworks/Inventory/db.py
@@ -9,7 +9,24 @@ conn = psycopg2.connect(dbname=DB_name, user=USER, password=PASSWORD, host=HOST)
 cur = conn.cursor()
 
 def get_products():
-    cur.execute("SELECT * FROM products;")
+    cur.execute("SELECT * FROM products ORDER BY id;")
     products = cur.fetchall()
     for product in products:
         print(product)
+
+def add_product(name, quantity, price):
+    cur.execute("INSERT INTO products(name, quantity, price) VALUES (%s, %s, %s) RETURNING id;",
+                (name, quantity, price))
+    product_id = cur.fetchone()[0]
+    conn.commit()
+    print(f"Product added with id: {id}")
+
+def delete_product (product_id):
+    cur.execute("DELETE FROM products WHERE id = %s", (product_id,))
+    conn.commit()
+    print(f"Product with id {product_id} deleted successfully")
+
+def update_product(product_id, name, quantity, price):
+    cur.execute("UPDATE products SET name = %s, quantity = %s, price = %s WHERE id = %s;",(name, quantity, price, product_id))
+    conn.commit()
+    print(f"Product with id {product_id} updated successfully")
--- Dana homeworks/Inventory/main.py
+++ Dana homeworks/Inventory/main.py
@@ -1,4 +1,35 @@
 from db import *
 
 if __name__ == "__main__":
-    get_products()
+    while True:
+        print("\nInventory Management Menu")
+        print("1. List products")
+        print("2. Add product")
+        print("3. Delete product")
+        print("4. Update product")
+        print("X. Exit")
+
+        choice = input ("Enter your choice:")
+
+        if choice == "1":
+            get_products()
+
+        elif choice == "2":
+            name = input ("Product name :")
+            quantity = int(input("Quantity: "))
+            price = float(input("Price: "))
+            add_product(name, quantity, price)
+
+        elif choice == "3":
+            product_id = int(input("Product_id: "))
+            delete_product(product_id)
+
+        elif choice == "4":
+            product_id = int(input("Product:id: "))
+            name = input ("Product name :")
+            quantity = int(input("Quantity: "))
+            price = float(input("Price: "))
+            update_product(product_id, name, quantity, price)
+
+        elif choice.upper() == "X":
+            break 
