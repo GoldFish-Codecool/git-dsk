e8876225976aa5ba0423d622e0691bf5950c5c0d
DaniellaRigo <146843246+DaniellaRigo@users.noreply.github.com>
2023-11-07 08:51:34 +0000

    9th class

---
+++ Dana homeworks/Inventory/db.py
@@ -0,0 +1,9 @@
+import psycopg2
+
+DB_name = "xokifxcx"
+USER = "xokifxcx"
+PASSWORD = "7Rrpd7lF5MeFtiz6CLRG3PObvC9KSodl"
+HOST = "flora.db.elephantsql.com"
+
+conn = psycopg2.connect(dbname=DB_name, user=USER, password=PASSWORD, host=HOST)
+
