dc1c198896c0be8cf9bc339d29ced1182c7b34d2
DaniellaRigo <146843246+DaniellaRigo@users.noreply.github.com>
2023-11-07 09:08:13 +0000

    update

--- Dana homeworks/Inventory/db.py
+++ Dana homeworks/Inventory/db.py
@@ -6,4 +6,10 @@ PASSWORD = "7Rrpd7lF5MeFtiz6CLRG3PObvC9KSodl"
 HOST = "flora.db.elephantsql.com"
 
 conn = psycopg2.connect(dbname=DB_name, user=USER, password=PASSWORD, host=HOST)
+cur = conn.cursor()
 
+def get_products():
+    cur.execute("SELECT * FROM products;")
+    products = cur.fetchall()
+    for product in products:
+        print(product)
