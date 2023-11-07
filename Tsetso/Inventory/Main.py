a254f64cac469e1b4e77ac0a35cea5baf520c1a6
Tsvetoslav1988 <146844445+Tsvetoslav1988@users.noreply.github.com>
2023-11-07 11:43:14 +0200

    commit1

--- Tsetso/Inventory/Main.py
+++ Tsetso/Inventory/Main.py
@@ -1,5 +1,31 @@
+
 from db import * 
 
+<<<<<<< HEAD
+if __name__ == "__main__"
+    while True:
+        print("\Inventory Management menu")
+        print("1. List Products")
+        print("2. Add products")
+        print("X. Exit")
+
+        choice = input("Enter your choice:")
+
+        if choice == "1":
+            get_products()
+        elif choice == "2":
+            name = input("Product name: ")
+            quantity = int(input("Quantity: "))
+            price = float(input("Price: "))
+            add_product(name, quantity, price)
+        elif choice.upper() == "X":
+            break
+
+
+
+=======
 if __name__ == "__main__":
     get_products()
     
+
+>>>>>>> c269635d472aaf85807a4455eee524d614bd1373
