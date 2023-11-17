6d14e0e1d8be72ecb168de8508e18321d6291786
Hakkovacs <89679747+Hakkovacs@users.noreply.github.com>
2023-11-17 11:53:29 +0000

    commit status

---
+++ TicTacToe/check_win.py
@@ -0,0 +1,17 @@
+def win (table):
+
+    winner = False
+    if  (row[1][1] == row[1][3] == row[1][5]) or:
+        (row[3][1] = row[3][3] = row[3][5]) or:
+        (row[5][1] = row[5][3] = row[5][5]) or:
+        (row[1][1] = row[3][3] = row[5][5]) or:
+        (row[5][1] = row[3][3] = row[1][5]):
+        winner = True
+
+    return winner
+
+
+
+    
+
+
