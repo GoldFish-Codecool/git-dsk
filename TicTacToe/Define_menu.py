4a4d43b803a5ec762238af592aa6b8695e932050
PeterBese <146844597+PeterBese@users.noreply.github.com>
2023-11-17 10:43:00 +0000

    new version

---
+++ TicTacToe/Define_menu.py
@@ -0,0 +1,19 @@
+def show_menu():
+    print (1. Player A vs Player B)
+    print (2. Player vs Computer)
+    print (3. Computer vs Computer)
+
+def main():
+    while_true:
+    choice = input(Enter your choice: ")
+                   
+                    if choice =="1":
+                    print("You chose option 1.)    
+                    elif choice == "2"
+                    print (Option 2 is not ready yet, please select Option 1)
+                    break
+                    elif choice == "3"
+                    print (Option 3 is not ready yet, please select Option 1.)
+                    break
+                    else 
+                    print("Invalid choice, please enter a valid option.")
