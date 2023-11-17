00a2a3d4972620510274a93963ee706fd9b83fd1
SlaveykoDSK <146844642+SlaveykoDSK@users.noreply.github.com>
2023-11-17 14:40:52 +0200

    TTT 2.0
    * Check for win
    * Check for a full board
    * Print results

--- Hajra magyarok/TicTacToe/game.py
+++ Hajra magyarok/TicTacToe/game.py
@@ -1,3 +1,4 @@
+import os
 from board import Board
 from player import Player
 
@@ -14,10 +15,33 @@ class Game:
         self.initiate_game()
     
     def initiate_game(self):
-        self.player1 = Player('Player 1', 'X')
-        self.player2 = Player('Player 2', 'O')
+        self.player1 = Player(input('Player 1 name: '), 'X')
+        self.player2 = Player(input('Player 2 name: '), 'O')
         self.active_player = self.player1
-        self.message = f'{self.active_player.name} turn.'
+        self.message = f'{self.active_player.name} turn ({self.active_player.symbol}).'
+
+    def play_game(self):
+        while self.status:
+            # Clear the screen
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            # Print the board
+            self.board.display_board()
+            print(self.message)
+            move = self.get_player_move()
+
+            # Check if move is valid
+            if move in ['quit', 'q']:
+                print('Thanks for playing!\n')
+                self.status = False 
+            elif move in ['help', 'h']:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Valid moves are: A1, A2, A3, B1, B2, B3, C1, C2, C3\n')
+                input('Press Enter to continue...')
+            elif move in self.board.board.keys():
+                self.make_move(move)
+            else:
+                self.message = f'{move} is not a valid move. Please try again. \n\n{self.active_player.name} turn.'
 
     def get_player_move(self):
         move = input('What is your move (A1...C3): ').lower()
@@ -25,15 +49,51 @@ class Game:
         return move
     
     def make_move(self, move):
-        
+        # Check if move is valid
         if self.board.board[move] == '•':
+            # Make the move
             self.board.board[move] = self.active_player.symbol
 
-            if self.active_player == self.player1:
-                self.active_player = self.player2
+            # Check for a winner
+            if self.check_for_winners():
+                self.message = f'{self.active_player.name} wins!\n'
+                self.status = False
+                return
+            # If no winner check for draw
+            elif self.check_for_draw():
+                self.message = 'It\'s a tie!\n'
+                self.status = False
+                return
             else:
-                self.active_player = self.player1
-            self.message = f'{self.active_player.name} turn.'
+                # Switch players
+                if self.active_player == self.player1:
+                    self.active_player = self.player2
+                else:
+                    self.active_player = self.player1
+                self.message = f'{self.active_player.name} turn ({self.active_player.symbol}).'
 
+        # If move is not valid
         else:
-            self.message = f'{move} is not empty. Please try again. \n\n{self.active_player.name} turn.'
+            self.message = f'{move} is not empty. Please try again. \n\n{self.active_player.name} turn ({self.active_player.symbol}).'
+
+    def check_for_draw(self):
+        if '•' not in self.board.board.values():
+            return True
+
+    def check_for_winners(self):
+        winning_combinations = [
+            ['a1', 'a2', 'a3'],
+            ['b1', 'b2', 'b3'],
+            ['c1', 'c2', 'c3'],
+            ['a1', 'b1', 'c1'],
+            ['a2', 'b2', 'c2'],
+            ['a3', 'b3', 'c3'],
+            ['a1', 'b2', 'c3'],
+            ['a3', 'b2', 'c1']
+        ]
+
+        for combination in winning_combinations:
+            if self.board.board[combination[0]] == self.board.board[combination[1]] == self.board.board[combination[2]] != '•':
+                return True
+    
+
--- Hajra magyarok/TicTacToe/main.py
+++ Hajra magyarok/TicTacToe/main.py
@@ -1,32 +1,20 @@
 import os
 from game import Game
 
-game = Game()
-
 # Clear the screen
 os.system('cls' if os.name == 'nt' else 'clear')
 print('Welcome to Tic-Tac-Toe!\n')
-print(f'{game.player1.name} vs {game.player2.name}\n')
+
+# Create a new game
+game = Game()
+
+print(f'\n{game.player1.name} ({game.player1.symbol}) vs {game.player2.name} ({game.player2.symbol})\n')
 input('Press Enter to continue...')
 
-while game.status:
-  # Clear the screen
-  os.system('cls' if os.name == 'nt' else 'clear')
+# Start the game
+game.play_game()
 
-  game.board.display_board()
-  print(game.message)
-  move = game.get_player_move()
-
-  if move in ['quit', 'q']:
-    print('Thanks for playing!\n')
-    game.status = False
-  elif move in ['help', 'h']:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print('Valid moves are: A1, A2, A3, B1, B2, B3, C1, C2, C3\n')
-    input('Press Enter to continue...')
-  elif move in game.board.board.keys():
-    game.make_move(move)
-  else:
-    game.message = f'{move} is not a valid move. Please try again. \n\n{game.active_player.name} turn.'
-  
-
+# Clear the screen. Print final board. Print results.
+os.system('cls' if os.name == 'nt' else 'clear')
+game.board.display_board()
+print(game.message)
