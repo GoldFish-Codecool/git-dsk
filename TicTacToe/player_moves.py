b6b318ec7749ba77ba0eaba6057c4f5f2de7ddfb
dimitardilov <49170285+dimitardilov@users.noreply.github.com>
2023-11-17 12:34:31 +0200

    player_move

--- TicTacToe/player_moves.py
+++ TicTacToe/player_moves.py
@@ -4,3 +4,8 @@ def get_player_moves():
         if len(move) != 2 or move[0] not in "ABC" or move[1] not in "123":
             print("Invalid input. Please enter a row (A-C) and a column (1-3).")
             continue
+        row, col = ord(move[0]) - 65, int(move[1]) - 1
+        if board[row][col] != '.':
+                print("That space is already taken. Try again.")
+                continue
+        return row, col
