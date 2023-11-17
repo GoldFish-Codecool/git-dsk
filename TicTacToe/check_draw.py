ac18974d8e6bff6282a13fb64b7a554a63afe39f
PeterBese <146844597+PeterBese@users.noreply.github.com>
2023-11-17 13:18:30 +0000

    new

--- TicTacToe/check_draw.py
+++ TicTacToe/check_draw.py
@@ -1,10 +1,13 @@
-def drawn (table):
-    drawn = false
+def drawn(table):
+    drawn = True
+    for row in table:
+        for element in row:
+            if element == " ":
+                drawn = False
+                break
 
+        if not drawn:
+            break 
 
-for row in table:
-    for element in row:
-        if element == X or element == 0:
-            print("Drawn! Do you want to restart? (Y/N)")
-            
-        drawn = True
+    if drawn:
+        print("Drawn! Do you want to play again? Y/N")
