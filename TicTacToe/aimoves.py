c04f95dfc6504933b5e1e00226909438d247467d
Hakkovacs <89679747+Hakkovacs@users.noreply.github.com>
2023-11-18 14:22:23 +0000

    test

--- TicTacToe/aimoves.py
+++ TicTacToe/aimoves.py
@@ -1,25 +1,41 @@
 import random
 
-def get_computer_moves(table):
-available_moves = []
+# here we define the computer move
+def computer_move(board, computer_char):
+    
+    def check_potential_win_move(board, player_char):
+        for row in range(1, 6, 2):  
+            for col in range(1, 6, 2): 
+                if board[row][col] == '.': 
+                    board[row][col] = player_char 
+                    if win(board): 
+                        board[row][col] = '.' 
+                        return (row, col)  
+                    board[row][col] = '.' 
+    return None
 
-  
-for i in [1,3,5]:
-    
-           
-            row, col = 2 * (ord(move[0]) - ord('A')) + 1, 2 * (int(move[1]) - 1) + 1
-           
-           
-            if table[row][col] != '.':
-                print("That space is already taken. Try again.")
-                continue
-            break
-    
-    
+#define potential blocking moves
 
-my_list = [1, 2, 3, 4, 5]
-rand_element = random.choice(my_list)
-print(rand_element)
-    
-    
-    return row, col
+    def check_potential_block_move(board, opponent_char):
+        for row in range(1, 6, 2):  
+            for col in range(1, 6, 2):  
+                if board[row][col] == '.': 
+                    board[row][col] = opponent_char 
+                    if win(board):  
+                        board[row][col] = '.' 
+                        return (row, col)  
+                    board[row][col] = '.'  
+    return None
+
+    available_moves = [(r, c) for r in range(1, 6, 2) for c in range(1, 6, 2) if board[r][c] == '.']
+
+#define the winning moves
+    winning_move = check_potential_win_move(board, computer_char)
+    if winning_move:
+        return winning_move
+
+    blocking_move = check_potential_block_move(board, 'O' if computer_char == 'X' else 'X')
+    if blocking_move:
+        return blocking_move
+
+    return random.choice(available_moves) if available_moves else None
