a939d8c2fd7411dc16da14042afb867ddb9d4419
DaniellaRigo <146843246+DaniellaRigo@users.noreply.github.com>
2023-11-18 14:33:28 +0000

    updates

--- Diversity Rules (Team 3)/main.py
+++ Diversity Rules (Team 3)/main.py
@@ -9,7 +9,10 @@ from player import *
 # instantiate player
 # make it work in "general" and then invoke all methods from the area list
 
-print ("The Wheel of fortune… You are thinking of the next OC and your and your direct reports’ OKRs completion status, while you do the last cross-check of your smart casual appearance in front of the mirror. After a long sight, you refocus your attention to less pressing matters: the football match tonight between Bulgaria and Hungary. Who will win tonight? Who should you be cheering for? Do you even care? How should you behave? Who will you be talking to? You leave to Hyatt to meet everyone at 18:45 sharp, knowing that from this point on you have full control over how the events of tonight unfold – but you will also need to own the consequences, let them be personal or professional...") 
+print("\n\nWELCOME TO THE BEST GAME YOU EVER PLAYED!")
+print("HAVE FUN!")
+print("---")
+print ("\nThe Wheel of fortune… You are thinking of the next OC and your and your direct reports’ OKRs completion status, while you do the last cross-check of your smart casual appearance in front of the mirror. After a long sight, you refocus your attention to less pressing matters: the football match tonight between Bulgaria and Hungary. Who will win tonight? Who should you be cheering for? Do you even care? How should you behave? Who will you be talking to? You leave to Hyatt to meet everyone at 18:45 sharp, knowing that from this point on you have full control over how the events of tonight unfold – but you will also need to own the consequences, let them be personal or professional...") 
 
 player_name = input("Enter your name: ")
 player1 = Player(player_name)
@@ -57,7 +60,7 @@ while True: # should be while not_game-over condition
     old_area = new_area
     user_input, new_area = get_user_choice()
 
-    print(player1.location, user_input, old_area)  
+    #print(player1.location, user_input, old_area)  
     health_update = area_list[player1.location].attribute_updates [user_input]["health"]
     player1.update_health(health_update)
     print(f"Your health is {player1.health} ")
@@ -76,7 +79,7 @@ while True: # should be while not_game-over condition
     elif player1.alcohol > 3:
         print(f"Game over. Your alcohol level exceeds {player1.alcohol} promils. Your are drunk and in jail.")
         break
-    elif player1.chairman < -4:
+    elif player1.chairman < -5:
         print(f"Game over. Your favour with the Chairman reached {player1.chairman}. Chairman had enough of you. You are fired.")
         break
     elif player1.location == 23:
