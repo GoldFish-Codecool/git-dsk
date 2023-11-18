1692e804cb7f18c749a34be95c1707148194027d
PeterBese <146844597+PeterBese@users.noreply.github.com>
2023-11-18 14:13:43 +0000

    hello

---
+++ TicTacToe/leaderboardpeter.py
@@ -0,0 +1,45 @@
+import psycopg2
+
+DB_NAME ="hucodjek"
+USER = "hucodjek"
+PASSWORD = "Eryt_3nZU6fPM3tO1Q11735_FJIvBAys"
+HOST = "flora.db.elephantsql.com"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=USER, password=PASSWORD, host=HOST)
+cur = conn.cursor()
+
+def get_winners() :
+    cur.execute("SELECT * FROM tictactoe ORDER BY id;")
+    tictactoe = cur.fetchall()
+    for winner in tictactoe :
+        print(winner)
+
+def add_winner(winner, opponent, xoro) :
+    cur.execute("INSERT INTO tictatoe (winner, opponent, xoro) VALUES (%s, %s, %s) RETURNING iD;", (winner, opponent, xoro))
+    winner_id = cur.fetchone()[0]
+    conn.commit()
+    print (f"Winner added with id: {product_id}")
+
+    conn.commit()
+    print(f"Winner with id {winner_id} updated successfully")
+
+    from dbPostGre import *
+
+if __name__ == "__main__":
+    
+    while True:
+        print("\nList of Winners Menu")
+        print("1. List winners")
+        print("2. Add winner")
+        print("X. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1": 
+            get_winners()
+        elif choice == "2" :
+            name = input("Winner’s name: ")
+            quantity = input("Opponent: ")
+            price = input("xoro: ")
+            add_winner(winner, opponent, xoro)
+            break
