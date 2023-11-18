c1b17c453b95c7245d44bff380785aa66a9207d5
PRanDSK <146845169+PRanDSK@users.noreply.github.com>
2023-11-18 11:03:18 +0200

    update

--- Diversity Rules (Team 3)/main.py
+++ Diversity Rules (Team 3)/main.py
@@ -9,20 +9,32 @@ from player import *
 # instantiate area list
 # instantiate player
 # make it work in "general" and then invoke all methods from the area list
+
+player_name = input("Etner your name: ")
+player1 = Player(player_name)
+print(f"Welcome {player_name} ")
+print(f"Your health is {player1.health} ")
+print(f"Your alcohol level is {player1.alcohol} ")
+print(f"Your favor with the Chairman is {player1.chairman} ")
+player1.update_health(-1)
+player1.update_alcohol(1)
+player1.update_chairman(-1)
+print(f"Your health is {player1.health} ")
+print(f"Your alcohol level is {player1.alcohol} ")
+print(f"Your favor with the Chairman is {player1.chairman} ")
+
 def get_user_choice():
           
     while True:
-        choices = area_list[0].choices # we need to put player location for the others
-        next_area = area_list[0].next_area
+        choices = area_list[player1.location].choices # we need to put player location for the others
+        next_area = area_list[player1.location].next_area
         for i in range(len(choices)):
             print (i+1, choices [i]) 
         print("Select a choice:")
-        #for key, value in choices.items():
-            #print(f"{key}: {value}")
-
+        
         user_input = input("Enter the number of your choice: ")
 
-        if user_input in choices:
+        if user_input in range(len(choices)):
             return choices[user_input]
         else:
             print("Invalid choice. Please try again.")
