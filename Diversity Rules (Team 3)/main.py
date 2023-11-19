3d8c5cfc9f5cd7744acc9a1d14aef279bba14dea
Tsvetoslav1988 <146844445+Tsvetoslav1988@users.noreply.github.com>
2023-11-19 09:50:37 +0200

    commit

--- Diversity Rules (Team 3)/main.py
+++ Diversity Rules (Team 3)/main.py
@@ -10,6 +10,7 @@ from player import *
 # make it work in "general" and then invoke all methods from the area list
 
 print("\n\n ⚽ WELCOME TO THE BEST GAME YOU EVER PLAYED⚽!")
+print("\nCreated by Diversity Rules Team")
 print("\nHAVE FUN!")
 print("---")
 print ("\n\nThe Wheel of fortune… You are thinking of the next OC and your and your direct reports’ OKRs completion status, while you do the last cross-check of your smart casual appearance in front of the mirror. \nAfter a long sight, you refocus your attention to less pressing matters: the football match tonight between Bulgaria and Hungary. \nWho will win tonight? Who should you be cheering for? How should you behave? Who will you be talking to? \nYou leave to Hyatt, knowing that from this point on you have full control over how the events of tonight unfold – but you will also need to own the consequences, let them be personal or professional...") 
